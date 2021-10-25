95537c9ab055404a10341300394def08408ca65e
Feuillastre Donnovan <donnovan42320@gmail.com>
2021-10-25 17:59:52 +0200

    Init OpenGL

---
+++ opengl_shader/opengl_shader/main.cpp
@@ -0,0 +1,10 @@
+﻿#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main( int argc, char * argv[])
+{
+	glfwInit(); //Fonction de la bibliothèque GLFW
+	glewInit(); //Fonction de la bibliothèque GLEW
+	glGetString(GL_VERSION); //Fonction de la bibliothèque OpenGL du système
+	return 0;
+}
